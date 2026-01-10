874d0f8330e06659c87bfad6bb54ce782cae221e
Matej Kubalos <matej.kubalos@email.cz>
2026-01-10 18:19:35 +0100

    Controller
    Created ZapasovnikController

---
+++ source/Zapasovnik.API/Controllers/ZapasovnikController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Zapasovnik.API.Entities;
+
+namespace Zapasovnik.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ZapasovnikController : ControllerBase
+    {
+        public List<League> Leagues { get; set; }
+        public List<Match> Matches { get; set; }
+        public List<Team> Teams { get; set; }
+        public List<TeamMatch> TeamsMatches { get; set; }
+        public List<Player> Players { get; set; }
+        public List<TeamPlayer> TeamsPlayers { get; set; }
+        public List<TeamPlayerArchive> TeamsPlayersArchive { get; set; }
+        public List<User> Users { get; set; }
+        public List<UserFavPlayer> UsersFavPlayers { get; set; }
+        public List<UserFavMatch> UsersFavMatches { get; set; }
+        public List<UserFavTeam> UsersFavTeams { get; set; }
+
+
+        public dbZapasovnikContext DbContext { get; set; }
+
+        public ZapasovnikController()
+        {
+            DbContext = new();
+            Leagues = DbContext.Leagues.ToList();
+            Matches = DbContext.Matches.ToList();
+            Teams = DbContext.Teams.ToList();
+            TeamsMatches = DbContext.TeamsMatches.ToList();
+            Players = DbContext.Players.ToList();
+            TeamsPlayers = DbContext.TeamsPlayers.ToList();
+            TeamsPlayersArchive = DbContext.TeamsPlayersArchive.ToList();
+            Users = DbContext.Users.ToList();
+            UsersFavPlayers = DbContext.UsersFavPlayers.ToList();
+            UsersFavMatches = DbContext.UsersFavMatches.ToList();
+            UsersFavTeams = DbContext.UsersFavTeams.ToList();
+        }
+
+        [HttpGet(Name = "GetLeague")]
+        public IEnumerable<League> Get()
+        {
+            return Leagues.ToArray();
+        }
+    }
+}
