04a843554277197ee2a6e91ff2934950626db80f
Kulosh <matej.kubalos@email.cz>
2026-01-10 22:00:31 +0100

    Testing controller - POST

--- source/Zapasovnik.API/Controllers/TestController.cs
+++ source/Zapasovnik.API/Controllers/TestController.cs
@@ -2,8 +2,8 @@
 
 namespace Zapasovnik.API.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class TestController : ControllerBase
     {
         public List<Test> Tests { get; set; }
@@ -18,6 +18,16 @@ namespace Zapasovnik.API.Controllers
         [HttpGet(Name = "GetTest")]
         public IEnumerable<Test> Get()
         {
+            return Tests.ToArray();
+        }
+
+        [HttpPost(Name = "PostTest")]
+        public IEnumerable<Test> Post(string subject)
+        {
+            Test newTest = new Test { Subject = subject };
+            DbContext.Tests.Add(newTest);
+            DbContext.SaveChanges();
+            Tests = DbContext.Tests.ToList();
             return Tests.ToArray();
         }
     }
