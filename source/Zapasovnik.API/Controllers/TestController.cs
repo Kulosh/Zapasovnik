5512f9e8bdb53d23aed692bf07e6b7f0cac93491
Matej Kubalos <matej.kubalos@email.cz>
2026-01-10 16:24:16 +0100

    DbContext and Test DataSet
    - Connected API to DB
    - Created Test class for data
    - Loaded test data

---
+++ source/Zapasovnik.API/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zapasovnik.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public List<Test> Tests { get; set; }
+        public dbZapasovnikContext DbContext { get; set; }
+
+        public TestController()
+        {
+            DbContext = new();
+            Tests = DbContext.Tests.ToList();
+        }
+
+        [HttpGet(Name = "GetTest")]
+        public IEnumerable<Test> Get()
+        {
+            return Tests.ToArray();
+        }
+    }
+}
---
+++ source/Zapasovnik.API/dbZapasovnikContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Zapasovnik.API
+{
+    public class dbZapasovnikContext : DbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=db.kulosh.eu;Port=20533;database=dbZapasovnik;uid=dbZapasovnik;password=wxm4mup0BVF4dzr-xam");
+        }
+    }
+}
