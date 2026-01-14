0e64a65e3d17c76c6b104ddfc600a625b403ca65
Kulosh <matej.kubalos@email.cz>
2026-01-14 23:12:32 +0100

    feat: Header and button
    - Added header and button for UI design purpose
    - Button *NON FUNCTIONAL*

--- source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/MainActivity.kt
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/MainActivity.kt
@@ -3,27 +3,39 @@ package com.example.zapasovnik
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.zapasovnik.ui.theme.ZapasovnikTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ZapasovnikTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier
+                    .background(color = MaterialTheme.colorScheme.background)
+                    .windowInsetsPadding(WindowInsets.statusBars)
+                ) {
+                    ZapasovnikHomepage(modifier = Modifier.fillMaxSize())
                 }
             }
         }
@@ -31,17 +43,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ZapasovnikHomepage(modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Zapasovnik",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+                .padding(20.dp)
+        )
+
+        Button(onClick = {}, modifier = Modifier.padding(20.dp)) {
+            Text(
+                text = "Matches"
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview (showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ZapasovnikTheme {
-        Greeting("Android")
+fun ZapasovnikPreview(){
+    Scaffold(modifier = Modifier) {
+        ZapasovnikHomepage(Modifier.fillMaxSize())
     }
 }
