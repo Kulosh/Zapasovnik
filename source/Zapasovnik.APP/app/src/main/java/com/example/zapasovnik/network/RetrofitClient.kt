6ad06d5066419999ee5ce79360172c4b08cfe6c8
Kulosh <matej.kubalos@email.cz>
2026-01-18 19:31:27 +0100

    update: API connection

---
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/network/RetrofitClient.kt
@@ -0,0 +1,20 @@
+package com.example.zapasovnik.network
+
+import com.google.firebase.appdistribution.gradle.ApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.kulosh.eu/zapasovnik"
+
+    val api: Api by lazy {
+        Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+            .create(Api::class.java)
+    }
+}
