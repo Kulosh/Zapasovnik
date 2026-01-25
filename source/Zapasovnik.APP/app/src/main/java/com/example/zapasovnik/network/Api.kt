a7e4929f412ef2bef90fbfc5180540d5d3b41430
Matej Kubalos <matej.kubalos@email.cz>
2026-01-25 21:50:38 +0100

    update:  POST user

--- source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/network/Api.kt
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/network/Api.kt
@@ -1,9 +1,14 @@
 package com.example.zapasovnik.network
 
 import com.example.zapasovnik.model.Match
+import com.example.zapasovnik.model.User
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface Api {
     @GET("TeamMatches")
     suspend fun getTeamMatches(): List<Match>
+
+    @POST("Users")
+    suspend fun postUser(): User
 }
