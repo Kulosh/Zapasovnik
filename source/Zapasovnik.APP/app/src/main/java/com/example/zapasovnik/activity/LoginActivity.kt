6454bcbbacd3c548f55abf937cf78e30886e44d8
Matej Kubalos <matej.kubalos@email.cz>
2026-01-25 19:56:00 +0100

    update: loginView created
    Created layout and activity,

---
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.zapasovnik.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import com.example.zapasovnik.R
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.login_layout)
+    }
+}
