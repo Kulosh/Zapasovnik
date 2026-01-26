4b67f5cb0d9dd134edaa8269d26dc3d49a0aaf5a
Matej Kubalos <matej.kubalos@email.cz>
2026-01-26 17:18:25 +0100

    update: LoginAcitivity
    - loading values from inputs
    - checking if inputs aren't empty
    - checking server response for user existence

--- source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/LoginActivity.kt
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/LoginActivity.kt
@@ -3,9 +3,15 @@ package com.example.zapasovnik.activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
 import com.example.zapasovnik.R
+import com.example.zapasovnik.model.User
+import com.example.zapasovnik.network.RetrofitClient
+import kotlinx.coroutines.launch
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +21,24 @@ class LoginActivity : ComponentActivity() {
 
         val loginBtn = findViewById<Button>(R.id.loginBtn)
         loginBtn.setOnClickListener {
+            val userName: String = findViewById<EditText>(R.id.usernameLogin).text.toString()
+            val password: String = findViewById<EditText>(R.id.passwordLogin).text.toString()
+            val intent = Intent(this, HomeActivity::class.java)
 
+            if (userName != "" && password != "") {
+                lifecycleScope.launch {
+                    val user = User(username =  userName, password = password)
+                    val response: Boolean = RetrofitClient.api.postUser(user)
 
-//            val intent = Intent(this, HomeActivity::class.java)
-//            startActivity(intent)
+                    if (response) {
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(applicationContext, "Invalid login", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(applicationContext, "Invalid login", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
