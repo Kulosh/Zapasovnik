794c0e72bfec621a67ee45e779ab912b756e27f2
Matej Kubalos <matej.kubalos@email.cz>
2026-01-31 17:26:59 +0100

    update: ProfileActivity
    - Imports
    - ContentView from home_layout to profile_layout

--- source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/ProfileActivity.kt
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/ProfileActivity.kt
@@ -1,15 +1,21 @@
 package com.example.zapasovnik.activity
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
 import com.example.zapasovnik.R
+import com.example.zapasovnik.network.RetrofitClient
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.buildJsonObject
 
 class ProfileActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.home_layout)
+        setContentView(R.layout.profile_layout)
 
     }
 }
