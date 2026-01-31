5c1725aca8f2d2a611d888e1dd19df09f2d03627
Matej Kubalos <matej.kubalos@email.cz>
2026-01-31 12:39:56 +0100

    update: Activity and layout file init

---
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/ProfileActivity.kt
@@ -0,0 +1,15 @@
+package com.example.zapasovnik.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import com.example.zapasovnik.R
+
+class ProfileActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.home_layout)
+
+    }
+}
