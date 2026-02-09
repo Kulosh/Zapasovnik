8d399c6c4870e17fb09e8ca373e2d0638df6c2d9
Kulosh <matej.kubalos@email.cz>
2026-02-09 20:36:22 +0100

    update: tableAdapter

---
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/viewModel/PlayersTableAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.zapasovnik.viewModel
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.zapasovnik.R
+import com.example.zapasovnik.model.FavPlayer
+
+class PlayersTableAdapter(private val players: List<FavPlayer>):
+    RecyclerView.Adapter<PlayersTableAdapter.PlayerTableHolder>() {
+        class PlayerTableHolder(view: View): RecyclerView.ViewHolder(view) {
+            val fname: TextView = view.findViewById(R.id.FavPlayerFname)
+            val lname: TextView = view.findViewById(R.id.FavPlayerLname)
+            val team: TextView = view.findViewById(R.id.FavPlayerTeam)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerTableHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fav_players_table_row, parent, false)
+            return PlayerTableHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: PlayerTableHolder, position: Int) {
+            val player = players[position]
+            holder.fname.text = player.FName
+            holder.lname.text = player.LName
+            holder.team.text = player.Team
+        }
+
+        override fun getItemCount() = players.size
+}
