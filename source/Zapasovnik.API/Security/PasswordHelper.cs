ebcd47fd5363219b673a1850fc216045087dcf1b
havrlikjakub <havrlikjakub@L114PC13>
2026-02-09 10:49:38 +0100

    Hashovani

---
+++ source/Zapasovnik.API/Security/PasswordHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace Zapasovnik.API.Security
+{
+    public class PasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                // převede byte[] na string (hex)
+                StringBuilder builder = new StringBuilder();
+
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
+}
