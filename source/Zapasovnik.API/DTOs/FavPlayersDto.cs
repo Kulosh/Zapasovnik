f7b67fc89df32dae792b04f2b8b0de7aadff3ad0
Kulosh <matej.kubalos@email.cz>
2026-02-09 20:35:21 +0100

    rollback: No diffs

--- source/Zapasovnik.API/DTOs/FavPlayersDto.cs
+++ source/Zapasovnik.API/DTOs/FavPlayersDto.cs
@@ -4,6 +4,6 @@
     {
         public string FName { get; set; }
         public string LName { get; set; }
-        public string? Team { get; set; } = "No team";
+        public string? Team { get; set; }
     }
 }
