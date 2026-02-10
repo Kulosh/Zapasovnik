9a2fda0c0b0a8b6c9bb21264ee961421ad40bb8f
Kulosh <matej.kubalos@email.cz>
2026-02-10 20:29:22 +0100

    init: NewPlayer

---
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/NewPlayerActivity.kt
@@ -0,0 +1,13 @@
+package com.example.zapasovnik.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.zapasovnik.R
+
+class NewPlayerActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_player_layout)
+        
+    }
+}
