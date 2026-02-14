9f5d9e5de81ae0bbe94b0fa162159cd48c20284a
Matej Kubalos <matej.kubalos@email.cz>
2026-02-14 17:02:42 +0100

    update: Autocomplete on teams

--- source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/NewPlayerActivity.kt
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/NewPlayerActivity.kt
@@ -1,13 +1,45 @@
 package com.example.zapasovnik.activity
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.zapasovnik.R
+import com.example.zapasovnik.network.RetrofitClient
+import kotlinx.coroutines.launch
 
 class NewPlayerActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_player_layout)
-        
+
+        val teamSel = findViewById<AutoCompleteTextView>(R.id.newPlayerTeam)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, mutableListOf<String>())
+        teamSel.setAdapter(adapter)
+
+        teamSel.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                lifecycleScope.launch {
+                    val teams = RetrofitClient.api.getTeams().map { it.TeamName }
+//                    Log.d("Teams", "$teams")
+
+                    adapter.clear()
+                    adapter.addAll(teams)
+                    adapter.notifyDataSetChanged()
+
+                    if (teams.isNotEmpty() && !teamSel.isPopupShowing) {
+                        teamSel.showDropDown()
+                    }
+                }
+            }
+        })
     }
 }
