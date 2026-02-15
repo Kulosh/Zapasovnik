30d0dd240983c8d52e1ae6152361c543e2940ded
Matej Kubalos <matej.kubalos@email.cz>
2026-02-15 17:43:22 +0100

    update: MatchDetail class

---
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/model/MatchDetail.kt
@@ -0,0 +1,22 @@
+package com.example.zapasovnik.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MatchDetail (
+    @SerialName("matchId")
+    var Id: Int,
+
+    @SerialName("team1")
+    var Team1: String,
+
+    @SerialName("team2")
+    var Team2: String,
+
+    @SerialName("matchDate")
+    var Date: String,
+
+    @SerialName("league")
+    var League: String
+)
