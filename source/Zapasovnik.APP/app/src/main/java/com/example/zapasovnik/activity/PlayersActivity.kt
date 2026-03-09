876b8d2d9dec4ac67d3c0168c8a166ec0a19321a
Matej Kubalos <matej.kubalos@email.cz>
2026-03-09 21:09:53 +0100

    same as prev but on players

--- source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/PlayersActivity.kt
+++ source/Zapasovnik.APP/app/src/main/java/com/example/zapasovnik/activity/PlayersActivity.kt
@@ -9,15 +9,22 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.zapasovnik.R
+import com.example.zapasovnik.model.UserData
 import com.example.zapasovnik.network.RetrofitClient
 import com.example.zapasovnik.viewModel.PlayersTableAdapter
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class PlayersActivity : ComponentActivity() {
+
+    lateinit var userData: UserData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.players_list_layout)
+
+        userData = UserData(this)
 
         val recyclerView = findViewById<RecyclerView>(R.id.playersTableView)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -30,6 +37,9 @@ class PlayersActivity : ComponentActivity() {
 
         lifecycleScope.launch {
             val players = RetrofitClient.api.getPlayers()
+            val isAdmin = userData.adminFlow.first()
+
+            if (!isAdmin) addPlayer.visibility = Button.GONE
 
             recyclerView.adapter = PlayersTableAdapter(players) { player ->
                 val intent = Intent(this@PlayersActivity, PlayerDetailActivity::class.java)
